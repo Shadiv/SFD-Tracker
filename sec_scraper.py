92998bdfda341c94082a7de397bb019ede8e6479
Shadiv <wisenheim@mail.ru>
2022-08-09 17:05:23 +0200

    Update sec_scraper.py

--- sec_scraper.py
+++ sec_scraper.py
@@ -1,7 +1,18 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+
+ticker = 'FSLR'
 
 PATH = '/Users/ivanshaydulin/Personal/MyGit/SFD-Tracker/chrome_driver/chromedriver'
 driver = webdriver.Chrome(PATH)
-driver.get('https://www.sec.gov/')
+driver.get('https://www.sec.gov/edgar/search/')
 # driver.close() - close tab
 # driver.quit() - quit browser
+search = driver.find_element('id', 'entity-short-form')
+# element2 = driver.find_element_by_id('date-range-select') entity-full-form
+
+driver.implicitly_wait(10)
+
+search.send_keys(ticker)
+search.send_keys(Keys.RETURN)
